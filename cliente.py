56cb8309e665f8bf9cb18ddfb03c28cb89b51366
João Prates <jvdprates@hotmail.com>
2021-06-23 23:56:46 -0300

    Codificação e empacotamento

--- cliente.py
+++ cliente.py
@@ -1,17 +1,47 @@
-#!/usr/bin/env python3
-
 import socket
+import base64
+import struct
 
 HOST = '127.0.0.1'
-PORT = 3333
+PORT = 50000
 
 print("❗ Programa iniciado!")
-simpleSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-simpleSocket.connect((HOST,PORT))
 
-print('✅ Conectado com sucesso ao servidor! enviando mensagem...')
-simpleSocket.sendall(str.encode('Boa noite gente bonita!'))
-data = simpleSocket.recv(1024)
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+skt.connect((HOST, PORT))
+print('✅ Conectado com sucesso ao servidor!')
 
-print('💌 [Mensagem ecoada]:', data.decode())
+class Frame:
+    def __init__(self, sync1, sync2, length, checksum, _id, flags, data):
+        self.sync1 = sync1
+        self.sync2 = sync2
+        self.length = length
+        self.checksum = checksum
+        self._id = _id
+        self.flags = flags
+        self.data = data
+
+
+inputMsg = input("❗ Digite uma mensagem: ")
+
+# Criando uma classe do tipo Frame e instaciando suas variaveis
+frame = Frame(0xDCC023C2, 0xDCC023C2, len(inputMsg),
+              0, 0, 0, inputMsg.encode("UTF-8"))
+
+# Empacotando a mensagem de acordo com o tamanho de cada quadro
+packedMsg = struct.pack("!IIHHBBs", frame.sync1, frame.sync2,
+                        frame.length, frame.checksum, frame._id, frame.flags, frame.data)
+print("⚙ [Mensagem empacotada]:", packedMsg)
+
+# Codificando o enquadramento para base16
+codedPack = base64.b16encode(packedMsg)
+print("⚙ [Mensagem enquadrada]:", codedPack)
+
+# Enviando mensagem codificada para o servidor
+skt.sendall(codedPack)
+
+# Recebendo um eco do servidor
+data = skt.recv(1024)
+print('💌 [Mensagem ecoada]:', data)
+
 print('Operação realizada com sucesso, fechando programa... 😴')
--- servidor.py
+++ servidor.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python3
-
 import socket
+import base64
 
 HOST = 'localhost'
-PORT = 3333
+PORT = 50000
 
-simpleSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-simpleSocket.bind((HOST,PORT))
-simpleSocket.listen()
+print("❗ Programa iniciado!")
 
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+skt.bind((HOST, PORT))
+skt.listen()
 print("❗ Aguardando conexão de um cliente...")
-connection, address = simpleSocket.accept()
 
+connection, address = skt.accept()
 print("✅ Conectado em", address)
+
 while True:
     data = connection.recv(1024)
     if not data:
         print("Dados recebidos, fechando a conexão... 😴")
         connection.close()
         break
-    print("📩 [Mensagem recebida]:", data.decode())
+    print("📩 [Mensagem recebida]:", data.decode('UTF-8'))
+
+    # Decodificando a mensagem em base16
+    data = base64.b16decode(data)
+    print("⚙ [Mensagem decodificada]:", data)
+
+    # Ecoando a mensagem decodificada para o cliente
+    print("📩 Ecoando para o cliente...")
     connection.sendall(data)
-    
