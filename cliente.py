f2e24df0379969a340270f669d06aae9b55833a0
Bruno <b.holiveira99@gmail.com>
2021-06-26 18:10:12 -0300

    Comunicação estabelecida

--- cliente.py
+++ cliente.py
@@ -1,9 +1,18 @@
 import socket
 import base64
 import struct
+import sys
 
 HOST = '127.0.0.1'
 PORT = 50000
+
+SYNC_CODE = 0xDCC023C2
+
+def changeId(id):
+    if id == 0:
+        return 1
+    else:
+        return 0
 
 print("❗ Programa iniciado!")
 
@@ -26,32 +35,35 @@ print("inputMsg:", inputMsg)
 print("type(inputMsg):", type(inputMsg))
 
 # Criando uma classe do tipo Frame e instaciando suas variaveis
-frame = Frame(0xDCC023C2, 0xDCC023C2, len(inputMsg),
+frame = Frame(SYNC_CODE, SYNC_CODE, len(inputMsg),
               0, 0, 0, inputMsg.encode("UTF-8"))
+
 print("frame.data:", frame.data)
 print("type(frame.data):", type(frame.data))
 
-# longString = frame.sync1 + frame.sync2 + frame.length + frame.checksum + frame._id + frame.flags + frame.data
-# print("longString:", longString)
-# print("type(longString):", type(longString))
+message = bytearray(frame.data)
 
-# Empacotando a mensagem de acordo com o tamanho de cada quadro
-packedMsg = struct.pack("!2I2H2B{}s".format(frame.length), frame.sync1, frame.sync2,
-                        frame.length, frame.checksum, frame._id, frame.flags, frame.data)
+for byte in message:
+    byte = byte.to_bytes(1, sys.byteorder)
+    # Empacotando a mensagem de acordo com o tamanho de cada quadro
+    print("byte:", byte)
+    print("type(byte):", type(byte))
+    frame.length = sys.getsizeof(byte)
+    print("frame.length", frame.length)
+    packedMsg = struct.pack("!2I2H2Bs", frame.sync1, frame.sync2,
+                       frame.length, frame.checksum, frame._id, frame.flags, byte)
+    frame._id = changeId(frame._id)
+    print("⚙ [Mensagem empacotada]:", packedMsg)
 
-# for i in packedMsg:
-#     print(i, " - ", type(i))
-# print("⚙ [Mensagem empacotada]:", packedMsg)
+    # Codificando o enquadramento para base16
+    codedPack = base64.b16encode(packedMsg)
+    print("⚙ [Mensagem enquadrada]:", codedPack)
 
-# Codificando o enquadramento para base16
-codedPack = base64.b16encode(packedMsg)
-print("⚙ [Mensagem enquadrada]:", codedPack)
+    # Enviando mensagem codificada para o servidor
+    skt.send(codedPack)
 
-# Enviando mensagem codificada para o servidor
-skt.sendall(codedPack)
-
-# Recebendo um eco do servidor
-data = skt.recv(1024)
-print('💌 [Mensagem ecoada]:', data)
+    # Recebendo um eco do servidor
+    data = skt.recv(1024)
+    print('💌 [Mensagem ecoada]:', data)
 
 print('Operação realizada com sucesso, fechando programa... 😴')
--- servidor.py
+++ servidor.py
@@ -5,6 +5,8 @@ import struct
 HOST = 'localhost'
 PORT = 50000
 
+SYNC_CODE = 0xDCC023C2
+
 print("❗ Programa iniciado!")
 
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,6 +16,16 @@ print("❗ Aguardando conexão de um cliente...")
 
 connection, address = skt.accept()
 print("✅ Conectado em", address)
+
+def validateSync(decodedTuple):
+    sync1 = decodedTuple[0]
+    sync2 = decodedTuple[1]
+    print("Validação:", sync1, SYNC_CODE)
+
+    if sync1 == sync2 and sync1 == SYNC_CODE:
+        return True
+    else:
+        return False
 
 while True:
     data = connection.recv(1024)
@@ -26,21 +38,26 @@ while True:
     # Decodificando a mensagem em base16
     decodedData = base64.b16decode(data)
     print("⚙ [Mensagem decodificada]:", decodedData)
+    print("type(decodedData):", type(decodedData))
 
     # Unpack para regerar o tuple
-    tupleMsg = struct.unpack("!2I2H2B10s", decodedData)
+    tupleMsg = struct.unpack("!2I2H2Bs", decodedData)
     print("⚙ [Mensagem desempacotada]:", tupleMsg)
 
     for i in tupleMsg:
         print(i, " - ", type(i))
-
-    # Verificar os bytes de sincronização
-    if not (hex(tupleMsg[0]) == 0xdcc023c2):
-        print(hex(tupleMsg[0]))
-        print("Primeiro campo inválido")
+    
+    syncValidation = validateSync(tupleMsg)
+    if syncValidation:
+        packageId = tupleMsg[4]
+        packageData = tupleMsg[6]
+        
+        packedMsg = struct.pack("!2I2H2Bs", SYNC_CODE, SYNC_CODE,
+                       0, 0, packageId, 7, b'')        
+        # Ecoando a mensagem decodificada para o cliente
+        print("📩 Ecoando para o cliente...")
+        connection.send(packedMsg)
+    else:
+        print("Erro de validação, fechando a conexão... 😴")
         connection.close()
         break
-
-    # # Ecoando a mensagem decodificada para o cliente
-    # print("📩 Ecoando para o cliente...")
-    # connection.sendall(unpackedMsg)
